30f160d3ec044a525717d0ebe3efae8dd4f05f7e
Bilek-Petr <p.bilek29@gmail.com>
2024-10-03 17:57:52 +0200

    table header done

--- src/components/Table/TableHeader.jsx
+++ src/components/Table/TableHeader.jsx
@@ -6,20 +6,31 @@ export default function TableHeader({ table }) {
   return (
     <thead>
       {table.getHeaderGroups().map((headerGroup) => (
-        <tr key={headerGroup.id}>
+        <tr key={headerGroup.id} className="text-accentDark group">
           {headerGroup.headers.map((header) => (
-            <th key={header.id} width={header.getSize()} className="relative">
-              {flexRender(header.column.columnDef.header, header.getContext())}
-              {header.column.getCanSort() && (
-                <span>
-                  <FaSort onClick={header.column.getToggleSortingHandler()} />
-                </span>
-              )}
-              <div
-                onMouseDown={header.getResizeHandler()}
-                onTouchStart={header.getResizeHandler()}
-                className={`absolute right-0 top-0 h-full w-[4px] cursor-w-resize bg-red-500 ${header.column.getIsResizing() ? "w-2 bg-blue-500" : ""}`}
-              ></div>
+            <th key={header.id} width={header.getSize()}>
+              <div className="relative flex items-center justify-center gap-3 py-4">
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext(),
+                )}
+                {header.column.getCanSort() && (
+                  <span>
+                    <FaSort
+                      onClick={header.column.getToggleSortingHandler()}
+                      className="text-accentMedium hover:text-accentDark cursor-pointer transition duration-300"
+                    />
+                  </span>
+                )}
+                {/* Resizable Handle */}
+                <div
+                  onMouseDown={header.getResizeHandler()}
+                  onTouchStart={header.getResizeHandler()}
+                  className={`bg-accentMedium hover:bg-accentDark absolute -right-[0.1rem] top-1.5 h-4/5 w-[1.5px] cursor-w-resize opacity-0 group-hover:opacity-100 ${
+                    header.column.getIsResizing() ? "w-2 bg-blue-500" : ""
+                  } transition-opacity duration-300 ease-in-out`}
+                />
+              </div>
             </th>
           ))}
         </tr>
