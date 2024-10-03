3a2091ffe139b210b035964b1459d1ea32cae021
Bilek-Petr <p.bilek29@gmail.com>
2024-10-03 16:01:00 +0200

    simplified code, added filterChange when option is picked

--- src/components/FilterSelect.jsx
+++ src/components/FilterSelect.jsx
@@ -1,22 +1,27 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { columns } from "./ClientTable";
+import { FaFilter } from "react-icons/fa";
 
-export default function FilterSelect() {
-  const [selectedFilter, setSelectedFilter] = useState("");
-  const [filterOptions, setFilterOptions] = useState([]);
-
+export default function FilterSelect({
+  selectedFilter,
+  setSelectedFilter,
+  setColumnFilters,
+}) {
   const handleFilterChange = (e) => {
-    setSelectedFilter(e.target.value);
+    const newValue = e.target.value;
+    setSelectedFilter(newValue);
+
+    setColumnFilters([{ id: newValue, value: "" }]);
   };
 
   return (
     <div className="flex items-center justify-center">
       <label htmlFor="filter" className="font-bold">
-        Filtrovat dle:
+        <FaFilter className="text-gray-400" />
       </label>
       <select id="filter" value={selectedFilter} onChange={handleFilterChange}>
-        {columns.map((column, id) => (
-          <option value={column.accessorKey} key={id}>
+        {columns.map((column) => (
+          <option value={column.accessorKey} key={column.accessorKey}>
             {column.header}
           </option>
         ))}
