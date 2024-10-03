f10c41ecd5462cd2ed8115e531a38395ed22921c
Bilek-Petr <p.bilek29@gmail.com>
2024-10-03 17:08:36 +0200

    minor changes to api call

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,27 @@
 // API CALL to get the clients
 
 export const fetchClients = async () => {
-   try {
-      const response = await fetch(import.meta.env.VITE_API_URL, {
-         method: 'GET',
-         headers: {
-            Authorization:
-               'Basic ' +
-               btoa(`${import.meta.env.VITE_API_USER}:${import.meta.env.VITE_API_KEY}`),
-            'X-Instance-Name': import.meta.env.VITE_INSTANCE_NAME,
-         },
-      });
+  try {
+    const response = await fetch(import.meta.env.VITE_API_URL, {
+      method: "GET",
+      headers: {
+        Authorization:
+          "Basic " +
+          btoa(
+            `${import.meta.env.VITE_API_USER}:${import.meta.env.VITE_API_KEY}`,
+          ),
+        "X-Instance-Name": import.meta.env.VITE_INSTANCE_NAME,
+      },
+    });
 
-      if (!response.ok) {
-         throw new Error('The fetching process of client list has failed');
-      }
+    if (!response.ok) {
+      throw new Error("The fetching process of client list has failed");
+    }
 
-      const fetchedData = await response.json();
+    const fetchedData = await response.json();
 
-      return fetchedData.data;
-   } catch (error) {
-      console.error(error);
-   }
+    return fetchedData.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
