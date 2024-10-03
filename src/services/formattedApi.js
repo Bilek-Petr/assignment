086d664081239727d663d691667b00e4538a8acb
Bilek-Petr <p.bilek29@gmail.com>
2024-10-03 17:06:16 +0200

    fixed bug with empty fetching

--- src/services/formattedApi.js
+++ src/services/formattedApi.js
@@ -1,18 +1,20 @@
-//formatted API for table displaying
-
 import { fetchClients } from "./api";
 
 export const getFormattedClients = async () => {
   try {
     const clientData = await fetchClients();
-    //just get data that will be displayed in the table
+
+    // Just to get data that will be displayed in the table
     return clientData.map((client) => ({
-      name: client.name,
-      rating: client.rating,
-      ownerFullName: client.owner.fullName,
-      regNumber: client.regNumber,
-      city: client.primaryAddress.address.city,
-      category: client.category.value,
+      name: client?.name || "Unknown",
+      rating: client?.rating || "",
+      ownerFullName: client?.owner?.fullName || "",
+      regNumber: client?.regNumber || "",
+      city: client?.primaryAddress?.address?.city || "",
+      category: client?.category?.value || "",
+
+      // to trigger error.jsx uncomment the code below
+      // category: client?.category.value
     }));
   } catch (error) {
     throw new Error("Fetch of clients has failed");
