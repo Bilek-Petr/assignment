95880c4aa01cabb3f7e1c973e225816667035a15
Bilek-Petr <p.bilek29@gmail.com>
2024-10-03 14:32:35 +0200

    minor changes

--- src/services/formattedApi.js
+++ src/services/formattedApi.js
@@ -1,20 +1,20 @@
 //formatted API for table displaying
 
-import { fetchClients } from './api';
+import { fetchClients } from "./api";
 
 export const getFormattedClients = async () => {
-   try {
-      const clientData = await fetchClients();
-      //just get data that will be displayed in the table
-      return clientData.map((client) => ({
-         name: client.name,
-         rating: client.rating,
-         ownerFullName: client.owner.fullName,
-         regNumber: client.regNumber,
-         city: client.primaryAddress.address.city,
-         category: client.category.value,
-      }));
-   } catch (error) {
-      throw new Error('Fetch of clients has failed');
-   }
+  try {
+    const clientData = await fetchClients();
+    //just get data that will be displayed in the table
+    return clientData.map((client) => ({
+      name: client.name,
+      rating: client.rating,
+      ownerFullName: client.owner.fullName,
+      regNumber: client.regNumber,
+      city: client.primaryAddress.address.city,
+      category: client.category.value,
+    }));
+  } catch (error) {
+    throw new Error("Fetch of clients has failed");
+  }
 };
