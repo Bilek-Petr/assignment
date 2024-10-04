6ed6203ef830325be22124898ba7ca42d530f5f4
Bilek-Petr <p.bilek29@gmail.com>
2024-10-04 11:07:18 +0200

    data setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-   theme: {
-      extend: {},
-   },
-   plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        mainLight: "#F2F2F2",
+        accentMedium: "#CCCCCC",
+        accentDark: "#858383",
+      },
+    },
+  },
+  plugins: [],
 };
