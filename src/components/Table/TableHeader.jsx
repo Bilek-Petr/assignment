864d667506bccde9555df8ada2c5cdaef220f7f2
Bilek-Petr <p.bilek29@gmail.com>
2024-10-04 11:42:31 +0200

    added styles to hide last resizable handle

--- src/components/Table/TableHeader.jsx
+++ src/components/Table/TableHeader.jsx
@@ -7,14 +7,14 @@ export default function TableHeader({ table }) {
     <thead>
       {table.getHeaderGroups().map((headerGroup) => (
         <tr key={headerGroup.id} className="group text-accentDark">
-          {headerGroup.headers.map((header, index) => (
+          {headerGroup.headers.map((header) => (
             <th
               key={header.id}
               style={{
                 width: `${header.column.getSize()}px`,
               }}
             >
-              <div className="relative flex items-center justify-center gap-3 py-4">
+              <div className="relative flex items-center gap-3 py-4">
                 {flexRender(
                   header.column.columnDef.header,
                   header.getContext(),
@@ -31,7 +31,7 @@ export default function TableHeader({ table }) {
                 <div
                   onMouseDown={header.getResizeHandler()}
                   onTouchStart={header.getResizeHandler()}
-                  className="absolute -right-[0.1rem] top-1.5 h-4/5 w-[1.5px] cursor-w-resize bg-accentMedium opacity-0 transition-opacity duration-300 ease-in-out hover:bg-accentDark group-hover:opacity-100"
+                  className={`absolute right-2 top-1.5 h-4/5 w-[1px] cursor-w-resize bg-accentMedium opacity-0 transition-opacity duration-300 ease-in-out hover:bg-accentDark group-hover:opacity-100 ${header.index === headerGroup.headers.length - 1 && "invisible"}`}
                 />
               </div>
             </th>
