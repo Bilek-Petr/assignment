2c2e79beb01a96af005b37e3e8acde531283854d
Bilek-Petr <p.bilek29@gmail.com>
2024-10-04 11:08:15 +0200

    fixed width for error status

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function ErrorDisplay({ message }) {
+export default function Error({ message }) {
   return (
-    <div className="border border-red-500 p-4 font-bold text-red-500">
+    <div className="m-auto w-3/4 border border-red-500 p-4 text-center font-bold text-red-500 sm:w-2/4 lg:w-1/4">
       <span>{`Error: ${message}`}</span>
     </div>
   );
