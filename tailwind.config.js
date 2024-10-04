61c94512a7a2a448f76421c8ceeb8a5823e3f03f
Bilek-Petr <p.bilek29@gmail.com>
2024-10-04 12:49:32 +0200

    added colors for categories

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ export default {
         mainLight: "#F2F2F2",
         accentMedium: "#CCCCCC",
         accentDark: "#858383",
+
+        vipBg: "#ffc2f0",
+        vipText: "#9b2d88",
+        growBg: "#cff2bf",
+        growText: "#376800",
+        deadBg: "#d0d4dd",
+        deadText: "#555c6b",
       },
     },
   },
