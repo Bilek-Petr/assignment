ae155da060aa03911d85830ffa695c6660365c7e
Bilek-Petr <p.bilek29@gmail.com>
2024-10-04 11:35:35 +0200

    made name column bold

--- src/components/Table/TableRow.jsx
+++ src/components/Table/TableRow.jsx
@@ -3,9 +3,21 @@ import TableCell from "./TableCell";
 
 export default function TableRow({ row }) {
   return (
-    <tr key={row.id}>
-      {row.getVisibleCells().map((cell) => (
-        <TableCell key={cell.id} cell={cell} />
+    <tr
+      key={row.id}
+      className="cursor-pointer bg-white shadow-sm transition-colors hover:bg-cyan-100"
+    >
+      {row.getVisibleCells().map((cell, index) => (
+        <td
+          key={cell.id}
+          className={`p-1 ${
+            cell.column.columnDef.accessorKey === "name" && "font-bold"
+          } ${index === 0 && "rounded-l-md"} ${
+            index === row.getVisibleCells().length - 1 && "rounded-r-md"
+          }`}
+        >
+          <TableCell key={cell.id} cell={cell} />
+        </td>
       ))}
     </tr>
   );
