6cd00ae749c0bd099c9c0d030b7e04deeb640642
Bilek-Petr <p.bilek29@gmail.com>
2024-10-05 10:09:22 +0200

    added onRowClick

--- src/components/Table/TableRow.jsx
+++ src/components/Table/TableRow.jsx
@@ -3,11 +3,12 @@ import TableCell from "./TableCell";
 import CategoryCell from "./CategoryCell";
 import { getCellClassNames } from "../../utilities.js/getCellClassNames";
 
-export default function TableRow({ row }) {
+export default function TableRow({ row, onRowClick }) {
   return (
     <tr
       key={row.id}
       className="cursor-pointer bg-white shadow-sm transition-colors hover:bg-cyan-100"
+      onClick={() => onRowClick(row)}
     >
       {row.getVisibleCells().map((cell, index) => {
         const isCategoryCell = cell.column.columnDef.accessorKey === "category";
