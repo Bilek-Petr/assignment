53278c9df535205f49ef851bf58fa1399d1db76f
Bilek-Petr <p.bilek29@gmail.com>
2024-10-05 10:12:01 +0200

    added hook for managing client Data - to avoid overusing states in clientTable

---
+++ src/hooks/manageClientData.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { getFormattedClients } from "../services/formattedApi";
+
+export default function useClientData() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [columnFilters, setColumnFilters] = useState([]);
+  const [selectedRowData, setSelectedRowData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const formattedData = await getFormattedClients();
+        setData(formattedData);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return {
+    data,
+    error,
+    loading,
+    columnFilters,
+    setColumnFilters,
+    selectedRowData,
+    setSelectedRowData,
+  };
+}
