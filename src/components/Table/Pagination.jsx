54fa3655b9394f421448ca5bb8d33c2d41609513
Bilek-Petr <p.bilek29@gmail.com>
2024-10-05 10:07:47 +0200

    fixed render and appearrance

--- src/components/Table/Pagination.jsx
+++ src/components/Table/Pagination.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 
 export default function Pagination({ table }) {
-  <div>
-    <span>
-      Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
-    </span>
-    <div>
-      <button
-        onClick={() => table.previousPage()}
-        disabled={!table.getCanPreviousPage()}
-      >
-        {"<"}
-      </button>
-      <button
-        onClick={() => table.nextPage()}
-        disabled={!table.getCanNextPage()}
-      >
-        {">"}
-      </button>
+  return (
+    <div className="flex items-center gap-4 rounded-md py-4">
+      <span className="text-sm font-bold">
+        Page {table.getState().pagination.pageIndex + 1} of{" "}
+        {table.getPageCount()}
+      </span>
+      <div className="flex space-x-2">
+        <button
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+          className={`rounded-md bg-cyan-500 px-2 py-0.5 text-white transition-colors duration-200 hover:bg-cyan-600 ${
+            !table.getCanPreviousPage() ? "cursor-not-allowed opacity-50" : ""
+          }`}
+        >
+          {"<"}
+        </button>
+        <button
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+          className={`rounded-md bg-cyan-500 px-2 py-0.5 text-white transition-colors duration-200 hover:bg-cyan-600 ${
+            !table.getCanNextPage() ? "cursor-not-allowed opacity-50" : ""
+          }`}
+        >
+          {">"}
+        </button>
+      </div>
     </div>
-  </div>;
+  );
 }
