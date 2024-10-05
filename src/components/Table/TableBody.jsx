f2af4bf53f1e2404755be81dca54a3d31183ede0
Bilek-Petr <p.bilek29@gmail.com>
2024-10-05 10:08:18 +0200

    added onRowClick

--- src/components/Table/TableBody.jsx
+++ src/components/Table/TableBody.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import TableRow from "./TableRow";
 
-export default function TableBody({ table }) {
+export default function TableBody({ table, onRowClick }) {
   return (
     <tbody>
       {table.getRowModel().rows.map((row) => (
-        <TableRow key={row.id} row={row} />
+        <TableRow key={row.id} row={row} onRowClick={onRowClick} />
       ))}
     </tbody>
   );
