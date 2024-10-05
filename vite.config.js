e853e0686863cb9959e91681c60296aa8a6cf29f
Bilek-Petr <p.bilek29@gmail.com>
2024-10-05 10:41:46 +0200

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+console.log(process.env);
+
 export default defineConfig({
   plugins: [react()],
   base: "/assignment/",
