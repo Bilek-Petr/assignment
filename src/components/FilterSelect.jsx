2a7643212838979385e844318ba687e18eb23931
Bilek-Petr <p.bilek29@gmail.com>
2024-10-05 10:06:41 +0200

    changed the import and fixed styling

--- src/components/FilterSelect.jsx
+++ src/components/FilterSelect.jsx
@@ -1,4 +1,4 @@
-import { columns } from "./ClientTable";
+import { clientTableColumns } from "./config/tableColumns";
 import { FaFilter } from "react-icons/fa";
 
 export default function FilterSelect({
@@ -14,12 +14,17 @@ export default function FilterSelect({
   };
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center gap-2">
       <label htmlFor="filter" className="font-bold">
         <FaFilter className="text-gray-400" />
       </label>
-      <select id="filter" value={selectedFilter} onChange={handleFilterChange}>
-        {columns.map((column) => (
+      <select
+        id="filter"
+        value={selectedFilter}
+        onChange={handleFilterChange}
+        className="rounded-full border border-gray-300 bg-white px-3 py-2 shadow-sm transition-all duration-300 ease-in-out hover:border-cyan-400 focus:border-cyan-400 focus:outline-none focus:ring-1 focus:ring-cyan-400"
+      >
+        {clientTableColumns.map((column) => (
           <option value={column.accessorKey} key={column.accessorKey}>
             {column.header}
           </option>
