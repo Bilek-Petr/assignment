162f5cdbb894d537ffe4f985e72ac98c2bf5bba6
Bilek-Petr <p.bilek29@gmail.com>
2024-10-05 10:11:20 +0200

    added config files to simplify the structure

---
+++ src/components/config/tableColumns.js
@@ -0,0 +1,25 @@
+export const clientTableColumns = [
+  {
+    accessorKey: "name",
+    header: "Název/Jméno",
+    size: 400,
+  },
+  {
+    accessorKey: "rating",
+    header: "Rating",
+    size: 100,
+  },
+  {
+    accessorKey: "ownerFullName",
+    header: "Vlastník",
+  },
+  {
+    accessorKey: "regNumber",
+    header: "IČO",
+  },
+  { accessorKey: "city", header: "Město" },
+  {
+    accessorKey: "category",
+    header: "Kategorie",
+  },
+];
